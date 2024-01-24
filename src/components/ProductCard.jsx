bdbf421008fca6d693ad01f8febfb5ca0e44061d
jeanCodeUnMax <webmanhtc@gmail.com>
2024-01-25 00:00:23 +0100

    feat: render card with MOCK in 2 columns

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,49 +1,35 @@
-/* eslint-disable react/prop-types */
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import PropTypes from 'prop-types';
-import productUrl from "./../assets/boisson-chaude_1.jpg";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import PropTypes from "prop-types";
+import "./ProductCard.css";
 
- 
+export default function ProductCard(props) {
+	const { product } = props;
 
-
-export default function ProductCard(props){
-
-
-const { product } = props; 
-
-
-console.log(product.image)
-
-return (
-
-    <Card style={{ width: '18rem' }}>
-      {/* <Card.Img variant="top" src={product.image} /> */}
-      <Card.Img variant="top" src={productUrl} />
-      <Card.Body>
-        <Card.Title>{product.name}</Card.Title>
-        <Card.Text>
-          {product.description}
-        </Card.Text>
-        <Card.Text>
-          {product.price}
-        </Card.Text>
-
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  );
-  
+	return (
+		<Card style={{ width: "18rem" }}>
+			<Card.Img
+				className="imageProduct"
+				variant="top"
+				src={`./src/assets/${product.image}`}
+			/>
+			<Card.Body>
+				<Card.Title>{product.name}</Card.Title>
+				<Card.Text>{product.description}</Card.Text>
+				<Card.Text>{product.price}</Card.Text>
+				<Button variant="primary">Ajouter au panier</Button>
+			</Card.Body>
+		</Card>
+	);
 }
 
-
-// définir les types 
+// définir les types
 ProductCard.propTypes = {
-  product: PropTypes.exact({
-    category: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired, 
-    image: PropTypes.string.isRequired, 
-  }).isRequired
-}
+	product: PropTypes.exact({
+		category: PropTypes.string.isRequired,
+		description: PropTypes.string.isRequired,
+		price: PropTypes.number.isRequired,
+		name: PropTypes.string.isRequired,
+		image: PropTypes.string.isRequired,
+	}).isRequired,
+};
--- src/pages/ChoiceProduct.jsx
+++ src/pages/ChoiceProduct.jsx
@@ -1,51 +1,17 @@
-/* eslint-disable no-prototype-builtins */
-// import ProductCard from "../components/ProductCard";
-// import mock from "../../mock.json";
+import ProductCard from "../components/ProductCard";
+import "./ChoiceProduct.css";
+import { useState } from "react";
 import superMock from "../mock.json";
-import axios from "axios";
-
-import { useEffect, useState } from "react";
 
 export default function ChoiceProduct() {
-	const [data, setData] = useState([]);
-	console.log("render de ChoiceProduct");
-
-	useEffect(() => {
-		console.log("composant monté");
-		// console.log(JSON.parse(superMock));
-		axios
-			.get(superMock)
-			.then((response) => setData(response))
-			.then((data) => console.log(data));
-
-		// .then((data) => console.log(data));
-	}, []);
-
+	const [data] = useState(superMock);
 	return (
 		<>
-			<p>La grande page</p>
-			{/* {data && data.map((item) => <li key={item.id}>item.category</li>)} */}
-
-			{/* <ProductCard key={index} product={product} /> */}
+			<h1>the Fat - Fooood</h1>
+			<div className="choiceProduct">
+				{data &&
+					data.map((item) => <ProductCard key={item.id} product={item} />)}
+			</div>
 		</>
 	);
 }
-
-// en dure pour le test :
-// const productTest = {
-// 	category: "burger",
-// 	name: "big-mac",
-// 	description:		"Un géant parmice spéciale. Prêt pour une expérience gastronomique démesurée ?, les burgers ! 12 onces de pur plaisir, fromage fondant, et sauce",
-
-// 	image: "https://loremflickr.com/cache/resized/3046_2568827589_706e13d0be_320_240_nofilter.jpg",
-// 	price: 9.99,
-// };
-
-// const productTest1 = {
-// 	category: "burger",
-// 	name: "big-moc",
-// 	description:		"Un géant parmice spéciale. Prêt pour une expérience gastronomique démesurée ?, les burgers ! 12 onces de pur plaisir, fromage fondant, et sauce",
-
-// 	image: "https://loremflickr.com/cache/resized/3046_2568827589_706e13d0be_320_240_nofilter.jpg",
-// 	price: 19.99,
-// };
