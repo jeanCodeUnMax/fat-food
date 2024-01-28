a3d3f9938ab65d0a7b61e778a248f777f039329e
Abdel Bailoul <ab.bailoul@gmail.com>
2024-01-28 03:13:00 +0100

    build: import bootstrap icons to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
